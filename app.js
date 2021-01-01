226f3d524d46aed5eb6f7c270f16158e615cdbd5
test <willyovcharuk1994@gmail.com>
2021-01-01 23:04:05 +0200

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const express = require('express')
 const config = require('config')
-
+const mongoose = require('mongoose')
 
 const app = express();
 
 const PORT = config.get('port')  || 5000;
 
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoURI"), {
+            useNewUrlParser: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        
+        app.listen(PORT, (err) => {
+            app
+            if (err) {
+                return console.error('Error in server setup: ', err);
+            }
+            console.log(`Server has been started on port ${PORT}...`);
+        })
+
+    } catch (err) {
+        console.error('Connecting to DB :', err.message);
+        process.exit(1);
+    }
+}
+
+start();
+
+
 app.get('/', (req, res) => {
     res.send('Hello world');
-    res.end()
+    res.end();
 })
 
-app.listen(PORT, (err) => {
-    app
-    if (err) {
-        return console.error('Error in server setup: ', err)
-    }
-    console.log(`Server has been started on port ${PORT}...`)
-})
